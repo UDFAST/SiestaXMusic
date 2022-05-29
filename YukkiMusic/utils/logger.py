53568e01352a1794f481d35e541b60ec5f0f0d0a
UDFAST <102271062+UDFAST@users.noreply.github.com>
2022-05-30 01:11:09 +0200

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -1,9 +1,9 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/MD-MXX/MV-UX > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/MD-MXX/MV-UX/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -20,15 +20,12 @@ async def play_logs(message, streamtype):
             chatusername = "Private Group"
         logger_text = f"""
 **YUKKI PLAY LOG**
-
 **Chat:** {message.chat.title} [`{message.chat.id}`]
 **User:** {message.from_user.mention}
 **Username:** @{message.from_user.username}
 **User ID:** `{message.from_user.id}`
 **Chat Link:** {chatusername}
-
 **Query:** {message.text}
-
 **StreamType:** {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
