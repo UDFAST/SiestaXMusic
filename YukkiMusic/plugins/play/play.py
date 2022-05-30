0520916ddab19bccd9712aeeb43f5cbb259c5ed7
UDFAST <102271062+UDFAST@users.noreply.github.com>
2022-05-30 07:34:11 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -12,6 +12,7 @@ import random
 import string
 from ast import ExceptHandler
 
+from strings.filters import command
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
                             Message)
